495d3540f5dc6f230c028e6813e3f40b70652305
FrancyPro <amd.info.2015@gmail.com>
2023-10-03 21:53:45 +0200

    Fix

--- src/main/java/gq/francypro149/survivalcore/Commands/MainCommand.java
+++ src/main/java/gq/francypro149/survivalcore/Commands/MainCommand.java
@@ -2,6 +2,7 @@ package gq.francypro149.survivalcore.Commands;
 
 import gq.francypro149.survivalcore.Main;
 import gq.francypro149.survivalcore.Utils.ColorUtil;
+import gq.francypro149.survivalcore.Utils.SpawnUtil;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.Command;
@@ -23,6 +24,15 @@ public class MainCommand implements CommandExecutor {
         if (args.length == 0) {
             audience.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#116903:#63C754><bold>SURVIVAL</bold></gradient> <gradient:#669160:#B0E6A8>"+Main.plugin.getDescription().getName()+" made by "+Main.plugin.getDescription().getAuthors().get(0)+" v"+Main.plugin.getDescription().getVersion()+"</gradient>"));
             return true;
+        } else if (args.length == 1 && args[0].equalsIgnoreCase("setspawn")) {
+            SpawnUtil.setSpawn(p.getLocation());
+            audience.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#116903:#63C754><bold>SURVIVAL</bold></gradient> <gradient:#669160:#B0E6A8>Hai settato lo spawn!</gradient>"));
+        } else if (args.length == 1 && args[0].equalsIgnoreCase("spawn")) {
+            if (!Main.plugin.getConfig().getString("Functions.Spawn.Setted").equalsIgnoreCase("true")) {
+                audience.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#116903:#63C754><bold>SURVIVAL</bold></gradient> <gradient:#669160:#B0E6A8>Lo spawn non è stato settato!</gradient>"));
+            }
+            p.teleport(SpawnUtil.location);
+            audience.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#116903:#63C754><bold>SURVIVAL</bold></gradient> <gradient:#669160:#B0E6A8>Sei stato teletrasportato allo spawn!</gradient>"));
         }
         return false;
     }
--- src/main/java/gq/francypro149/survivalcore/Utils/SpawnUtil.java
+++ src/main/java/gq/francypro149/survivalcore/Utils/SpawnUtil.java
@@ -8,30 +8,25 @@ import org.bukkit.World;
 import java.util.Objects;
 
 public class SpawnUtil {
-    private static Location location;
+    public static Location location;
 
     public static void getSpawn() {
-        String setted = Main.plugin.getConfig().getString("Functions.Spawn.Setted");
         World world = Main.plugin.getServer().getWorld(Objects.requireNonNull(Main.plugin.getConfig().getString("Functions.Spawn.world")));
         double x = Main.plugin.getConfig().getDouble("Functions.Spawn.x");
         double y = Main.plugin.getConfig().getDouble("Functions.Spawn.y");
         double z = Main.plugin.getConfig().getDouble("Functions.Spawn.z");
         float yaw = (float) Main.plugin.getConfig().getDouble("Functions.Spawn.yaw");
         float pitch = (float) Main.plugin.getConfig().getDouble("Functions.Spawn.pitch");
-        if (Objects.equals(setted, "true")) {
-            location = new Location(world, x, y, z, yaw, pitch);
-        } else {
-            Bukkit.getLogger().warning("Spawn non settato!");
-        }
+        location = new Location(world, x, y, z, yaw, pitch);
     }
 
-    public static void setSpawn() {
+    public static void setSpawn(Location setLocation) {
         Main.plugin.getConfig().set("Functions.Spawn.Setted", "true");
-        Main.plugin.getConfig().set("Functions.Spawn.world", location.getWorld().getName());
-        Main.plugin.getConfig().set("Functions.Spawn.x", location.getX());
-        Main.plugin.getConfig().set("Functions.Spawn.y", location.getY());
-        Main.plugin.getConfig().set("Functions.Spawn.z", location.getZ());
-        Main.plugin.getConfig().set("Functions.Spawn.yaw", location.getYaw());
-        Main.plugin.getConfig().set("Functions.Spawn.pitch", location.getPitch());
+        Main.plugin.getConfig().set("Functions.Spawn.world", setLocation.getWorld().getName());
+        Main.plugin.getConfig().set("Functions.Spawn.x", setLocation.getX());
+        Main.plugin.getConfig().set("Functions.Spawn.y", setLocation.getY());
+        Main.plugin.getConfig().set("Functions.Spawn.z", setLocation.getZ());
+        Main.plugin.getConfig().set("Functions.Spawn.yaw", setLocation.getYaw());
+        Main.plugin.getConfig().set("Functions.Spawn.pitch", setLocation.getPitch());
     }
 }
